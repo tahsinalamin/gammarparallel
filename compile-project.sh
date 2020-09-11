5b359dea4a7464eefc0c2be206dd8ee6c90aaba2
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-11 13:02:51 -0500

    Create compile-project.sh

---
+++ compile-project.sh
@@ -0,0 +1,7 @@
+#Author: Sikder Tahsin Al Amin
+#Project: gammarparallel
+#Description: build and compile the project gammaPartition.
+
+
+R CMD build gammaPartition
+R CMD INSTALL gammaPartition_1.0.tar.gz
