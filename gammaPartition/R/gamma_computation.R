4873d53db8b8b9a12b49659db40596c7757dbde0
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-11 15:04:19 -0500

    Create gamma_computation.R

---
+++ gammaPartition/R/gamma_computation.R
@@ -0,0 +1,50 @@
+## Author: Sikder Tahsin Al Amin
+## Project: gammarpartition (2020)
+## Description: Gamma matrix computation in blocks for input data set 
+## Output: gamma matrix in "partition_gamma_1.txt" file
+
+gamma.partition <- function(filename, sparse="Y") {
+  library(data.table)
+  library(chunkR)
+  gamma_cpp <- function(X, Flag1) {
+    .Call(`_gammaPartition_gamma_cpp`, X, Flag1)
+  }
+
+  print("**gamma computation started**")
+  #print(sparse)
+
+  #read the dataset
+  partial_gamma <- matrix() #gamma for each chunk
+  ChunkSize = 40 #set block size or chunk size
+  #ChunkSize = as.integer(sqrt(nrow(X_Matrix)))
+
+  t0= proc.time() #time starts
+  t_Start = Sys.time()
+  time_list = list()
+
+  tmp_path <- file.path(filename) #construct the path to a file
+  chunker_object <- chunker(tmp_path,chunksize = ChunkSize, sep=",", has_colnames = F, has_rownames = F) #create chunk object
+  iter = 1
+
+  while(next_chunk(chunker_object)){ #reading the chunk
+    chunk_table = get_table(chunker_object) #get the chunk
+    partial_gamma = gamma_cpp(t(as.matrix(chunk_table)), sparse) #pass the chunk to rcpp
+    if(iter==1){
+      NumDim = ncol(chunk_table)
+      full_gamma <- matrix(0,NumDim+1, NumDim+1) ##full gamma matrix initialized as zero
+    }
+    full_gamma = full_gamma + partial_gamma  #add partial gamma with the previous gamma
+
+    iter = iter + 1
+  }
+
+  #print(full_gamma)
+
+  t1=proc.time()
+  elapsed=t1-t0
+  print(elapsed)
+
+  write.table(full_gamma,"partition_gamma_1.txt",append=FALSE, sep=" ",row.names=FALSE, col.names=FALSE)
+
+  #print(paste0("Total blocks: ",iter))
+}
