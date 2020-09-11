19fae5e43733dd31b34fa116a85db1bf1e4b7f28
Sikder Tahsin Al-Amin <sal-amin2@uh.edu>
2020-09-11 13:13:00 -0500

    Delete compile-project.sh

--- compile-project.sh
+++
@@ -1,7 +0,0 @@
-#Author: Sikder Tahsin Al Amin
-#Project: gammarparallel
-#Description: build and compile the project gammaPartition.
-
-
-R CMD build gammaPartition
-R CMD INSTALL gammaPartition_1.0.tar.gz
